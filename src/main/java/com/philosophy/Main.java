96dcfba3b1b116409deabfac526f6f5e045e6b0a
IngramBJ <1478354316@qq.com>
2026-07-16 15:06:41 +0000

    建立项目

---
+++ src/main/java/com/philosophy/Main.java
@@ -0,0 +1,14 @@
+package com.philosophy;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+            "Philosophy Battle Engine Started"
+        );
+
+    }
+
+}
